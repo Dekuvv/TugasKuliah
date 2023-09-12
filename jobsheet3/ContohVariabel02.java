10081f266a63a2b7a38aff49dee3615281038c4a
Dekuvv <aditbagussadewa85@gmail.com>
2023-09-12 09:15:03 +0700

    as always

--- jobsheet3/ContohVariabel02.java
+++ jobsheet3/ContohVariabel02.java
@@ -2,14 +2,16 @@ public class ContohVariabel02 {
 
     public static void main(String[] args) {
         String salahSatuHobySayaAdalah = "bermain petak umpet";
-        boolean isPandai = true;
+        boolean Kalem = true;
         char jeniskelamin = 'L';
         byte _umurSayaSekarang = 19;
         double $ipk = 3.24, tinggi = 1.78;
+        int BeratBadan = 52;
         System.out.println(salahSatuHobySayaAdalah);
-        System.out.println("apakah pandai?" + isPandai);
+        System.out.println("Kalem Banget kah?" + Kalem);
         System.out.println("Jenis kelamin :" + jeniskelamin);
         System.out.println("Umurku saat ini :" + _umurSayaSekarang);
-        System.out.println(String.format("Saya beripk %s, dengan tinggi badan %s", $ipk, tinggi));
+        System.out.println("Berat badanku :" + BeratBadan);
+        System.out.println(String.format("Saya beripk %s, dengan tinggi badan %s, dan berbobot %s", $ipk, tinggi, BeratBadan));
     }
 }
