cc80bdfc73609b244c473e6a632205d44050bb9c
Dekuvv <aditbagussadewa85@gmail.com>
2023-10-10 08:18:15 +0700

    update

---
+++ WhileKelipatan02.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+public class WhileKelipatan02 {
+
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+
+        //Deklarasi
+        int kelipatan, jumlah=0, counter=0;
+
+        //Input
+        System.out.print("Masukkan bilangan kelipatan (1-9) : ");
+        kelipatan = scan.nextInt();
+
+        int i = 1;
+        while (i <= 50) {
+            i++;
+            if (i % kelipatan == 0) {
+                jumlah += i;
+                counter++;
+            }
+        }
+        System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
+        System.out.printf("Total Bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, jumlah);
+        scan.close();
+    }
+}
---
+++ jobsheet7/ForKelipatan02.java
@@ -0,0 +1,28 @@
+package jobsheet7;
+import java.util.Scanner;
+
+public class ForKelipatan02 {
+
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+
+        int kelipatan, jumlah=0, counter=0;
+
+        System.out.print("Masukkan bilangan kelipatan (1-9) : ");
+        kelipatan = scan.nextInt();
+
+        for (int i = 1; i <= 50; i++){
+            if ( i % kelipatan == 0) {
+                jumlah += i;
+                counter++;
+            }
+            
+        }
+        float avg = (float)jumlah / counter;
+        System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
+        System.out.printf("Total Bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, jumlah);
+        System.out.printf("Total Rata Rata jumlah dan counter %d adalah %.2f\n", kelipatan, avg);
+        scan.close();
+    }
+}
