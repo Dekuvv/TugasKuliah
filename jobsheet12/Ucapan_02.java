38327dbc708b822deec644b848df1a30a3160f87
Dekuvv <aditbagussadewa85@gmail.com>
2023-11-28 08:40:09 +0700

    update

---
+++ jobsheet12/Ucapan_02.java
@@ -0,0 +1,16 @@
+package jobsheet12;
+import java.util.Scanner;
+
+public class Ucapan_02 {
+    public static String PenerimaUcapan(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Tuliskan Nama orang yang ingin anda beri ucapan : ");
+        String namaOrang = sc.nextLine();
+        sc.close();
+        return namaOrang;
+    }
+    public static void main(String[] args) {
+        String nama = PenerimaUcapan();
+        System.out.println("Thank you " + nama + "\nMay the force with you.");
+    }
+}
