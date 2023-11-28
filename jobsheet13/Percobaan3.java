fec36af6a17a69df63c8ee391523f2c9e866f353
Dekuvv <aditbagussadewa85@gmail.com>
2023-11-28 09:05:09 +0700

    update

---
+++ jobsheet13/Percobaan3.java
@@ -0,0 +1,24 @@
+package jobsheet13;
+
+import java.util.Scanner;
+
+public class Percobaan3 {
+    static double hitungLaba(double saldo, int tahun) {
+        if (tahun == 0) {
+            return(saldo);
+        } else {
+            return(1.11 * hitungLaba(saldo, tahun - 1));
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Jumlah Saldo Awal : ");
+        int saldoAwal = sc.nextInt();
+        System.out.print("Lamanya Investasi : ");
+        int tahun = sc.nextInt();
+
+        System.out.println("Jumlah saldo setelah " + tahun + " tahun");
+        System.out.println(hitungLaba(saldoAwal, tahun));
+    }
+}
